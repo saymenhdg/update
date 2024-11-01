3e6a90354203e39cde40826c63afddba6c47a07c
saymenhdg <94260801+saymenhdg@users.noreply.github.com>
2024-11-02 00:16:04 +0800

    Add files via upload

---
+++ chillana/src/main/java/smweb/chillana/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package smweb.chillana.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import smweb.chillana.model.PostModel;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<PostModel, Integer> {
+    List<PostModel> getAllByUserId(int userId);
+}
